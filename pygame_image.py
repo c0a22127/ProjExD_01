93b35d664789c2a2bbf612282bbf7a67c7817f3d
kakeru hokida <teu_github@6mile.dev>
2023-09-26 07:57:17 +0000

    演習問題

--- pygame_image.py
+++ pygame_image.py
@@ -6,13 +6,13 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img_1 = pg.image.load("fig/pg_bg.jpg")
+    bg_img_2 = pg.image.load("fig/pg_bg.jpg")
+    bg_img_2 = pg.transform.flip(bg_img_2, True, False)
     koukaton = pg.image.load("fig/3.png")
     koukaton = pg.transform.flip(koukaton, True, False)
-    koukaton_routed = pg.transform.rotozoom(koukaton, 10, 1.0)
 
-    koukaton_lst = [koukaton, koukaton_routed]
-
+    koukaton_lst = [koukaton, pg.transform.rotozoom(koukaton, 20, 1.0) ]
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -20,9 +20,11 @@ def main():
         
         x = -(tmr % 1600)
 
-        screen.blit(bg_img, [x, 0])
-        screen.blit(bg_img, [1600 + x, 0])
-        screen.blit(koukaton_lst[tmr%2],[200, 300])
+        screen.blit(bg_img_1, [x, 0])
+        screen.blit(bg_img_2, [1600 + x, 0])
+
+        screen.blit(koukaton_lst[int(tmr / 50 % 2)], [200, 300])
+
         pg.display.update()
         tmr += 1        
         clock.tick(100)
